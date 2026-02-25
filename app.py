26196a985d887faa860455b0b54bdd5e2d143d69
youwin6926-lgtm <youwin6926@gmail.com>
2026-02-25 08:17:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ def home():
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    try:
-        data = request.get_json(force=True)
-        pm25 = float(data.get("pm25", 0))
+    data = request.get_json(force=True)
+    pm25 = float(data.get("pm25", 0))
 
         if pm25 <= 25:
             level = "ดี"
@@ -28,11 +27,7 @@ def analyze():
             "advice": advice
         })
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
-
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
+
 
