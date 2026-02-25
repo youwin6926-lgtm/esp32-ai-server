23ff680ec76403e9bfacf1774ec52fd17a61d346
youwin6926-lgtm <youwin6926@gmail.com>
2026-02-25 07:31:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,9 +31,8 @@ def analyze():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-
-# สำคัญมากสำหรับ Render
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
